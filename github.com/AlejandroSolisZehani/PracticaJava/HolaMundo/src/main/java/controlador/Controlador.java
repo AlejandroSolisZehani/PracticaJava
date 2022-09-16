5086b0a92ef8d0fca3713a7b0c443a595a902726
Alejandro Solis Zehani <alejandrosoliszehani@gmail.com>
2022-09-16 09:52:26 +0200

    Nuevo commit

---
+++ github.com/AlejandroSolisZehani/PracticaJava/HolaMundo/src/main/java/controlador/Controlador.java
@@ -0,0 +1,14 @@
+package controlador;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controlador {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Controlador.class, args);
+		System.out.println("Version 1.1");
+
+	}
+
+}
